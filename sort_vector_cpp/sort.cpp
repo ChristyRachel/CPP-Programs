3b89b69071a07a08ef9f0457680a3d1c9978447f
Christy Rachel Philip <40349694+ChristyRachel@users.noreply.github.com>
2020-08-11 21:40:07 -0400

    Add files via upload

---
+++ sort_vector_cpp/sort.cpp
@@ -0,0 +1,26 @@
+/*
+ * sort.cpp
+ *
+ *  Created on: Aug. 11, 2020
+ *      Author: christy
+ */
+
+
+// C++ program to sort a vector
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> v{ 1, 5, 8, 9, 6, 7, 3, 4, 2, 0 };
+
+    sort(v.begin(), v.end());
+
+    cout << "Sorted \n";
+    for (auto x : v)
+        cout << x << " ";
+
+    return 0;
+}
+
