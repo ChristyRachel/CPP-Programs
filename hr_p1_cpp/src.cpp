5d3afda40a1eb9d29a569cea66ae19d05362defd
Christy Rachel Philip <40349694+ChristyRachel@users.noreply.github.com>
2020-10-21 14:58:04 -0400

    print datatypes
    Input consists of the following space-separated values: int, long, char, float, and double, respectively.
    
    Output Format
    
    Print each element on a new line in the same order it was received as input. Note that the floating point value should be correct up to 3 decimal places and the double to 9 decimal places.
    
    Sample Input
    
    3 12345678912345 a 334.23 14049.30493
    
    Sample Output
    
    3
    12345678912345
    a
    334.230
    14049.304930000

---
+++ hr_p1_cpp/src.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    cin >> d;
+    cin >> e;
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout << fixed << d << endl;
+    cout << fixed << e << endl;
+    return 0;
+}
