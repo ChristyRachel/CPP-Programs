6c4f32ba8f4ab59b017c521977100755b91758c2
Christy Rachel Philip <40349694+ChristyRachel@users.noreply.github.com>
2020-12-27 16:52:13 -0800

    Day 0 of 30 Days Hackerrank challenge
    Problem
    Objective
    In this challenge, we review some basic concepts that will get you started with this series. You will need to use the same (or similar) syntax to read input and write output in challenges throughout HackerRank. Check out the Tutorial tab for learning materials and an instructional video!
    
    Task
    To complete this challenge, you must save a line of input from stdin to a variable, print Hello, World. on a single line, and finally print the value of your variable on a second line.
    
    You've got this!
    
    Note: The instructions are Java-based, but we support submissions in many popular languages. You can switch languages using the drop-down menu above your editor, and the inputString variable may be written differently depending on the best-practice conventions of your submission language.
    
    Input Format
    
    A single line of text denoting inputString (the variable whose contents must be printed).
    
    Output Format
    
    Print Hello, World. on the first line, and the contents of inputString on the second line.
    
    Sample Input
    
    Welcome to 30 Days of Code!
    
    Sample Output
    
    Hello, World. 
    Welcome to 30 Days of Code!
    
    Explanation
    
    On the first line, we print the string literal Hello, World.. On the second line, we print the contents of the inputString variable which, for this sample case, happens to be Welcome to 30 Days of Code!. If you do not print the variable's contents to stdout, you will not pass the hidden test case.

---
+++ HR_Day0/src.cpp
@@ -0,0 +1,31 @@
+/*
+ * src.cpp
+ *
+ *  Created on: Dec. 27, 2020
+ *      Author: christy
+ *      Ref: Hackerrank
+ */
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+    // Declare a variable named 'input_string' to hold our input.
+    string input_string;
+
+    // Read a full line of input from stdin (cin) and save it to our variable, input_string.
+    getline(cin, input_string);
+
+    // Print a string literal saying "Hello, World." to stdout using cout.
+    cout << "Hello, World." << endl;
+
+    // TODO: Write a line of code here that prints the contents of input_string to stdout.
+    cout << input_string << endl;
+
+    return 0;
+}
+
+
