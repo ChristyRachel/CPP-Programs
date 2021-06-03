696d1eae2d9112059a6503fdda2bb863e1568a99
Christy Rachel Philip <40349694+ChristyRachel@users.noreply.github.com>
2021-06-03 18:36:22 -0400

    Hacker Rank C++ Practice Section: Strings
    Problem link: https://www.hackerrank.com/challenges/c-tutorial-strings/problem

---
+++ HR_Challenge_CPP_STRINGS/src.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	// Complete the program
+  string a;
+  string b;
+  cin >> a;
+  cin >> b;
+
+  cout << a.length() << " " << b.length() << endl;
+  cout << a+b << endl;
+  swap( a[0], b[0]);
+  cout << a << " " << b << endl;
+    return 0;
+}
