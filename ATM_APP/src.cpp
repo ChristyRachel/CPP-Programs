6125e97484a1b0bff56cc315500eaca94a7c3740
Christy Rachel Philip <40349694+ChristyRachel@users.noreply.github.com>
2021-06-16 18:22:44 -0400

    ATM App in C++
    ATM App in C++

---
+++ ATM_APP/src.cpp
@@ -0,0 +1,53 @@
+/*
+ * src.cpp
+ *
+ *  Created on: Jun. 14, 2021
+ *      Author: christy
+ */
+
+#include <iostream>
+using namespace std;
+
+void show_menu() {
+	cout << "*******MENU*******" << endl;
+	cout << "1. Check Balance" << endl;
+	cout << "2. Deposit" << endl;
+	cout << "3. Withdraw" << endl;
+	cout << "******************" << endl;
+}
+
+int main() {
+
+	int option;
+	double balance = 1000;
+	double deposit;
+	double withdraw;
+
+	show_menu();
+	cout << "Select an option from these" << endl;
+
+while(cin >> option){
+	switch (option) {
+	case 1:
+		cout << "Your Balance is " << balance << "Rs." << endl;
+		break;
+	case 2:
+		cout << "Deposit Amount: " << endl;
+		cin >> deposit;
+		cout << "Your Current Balance is " << (balance + deposit) << endl;
+		break;
+	case 3:
+		cout << "Withdrawal Amount: " << endl;
+		cin >> withdraw;
+		if (balance > withdraw)
+			cout << "Your Current Balance is " << (balance - withdraw) << endl;
+		else
+			cout << "You don't have enough balance. Sorry!";
+		break;
+	default:
+		cout << "Invalid Option. Choose one from the above list."<< endl;
+	}
+}
+	return 0;
+}
+
