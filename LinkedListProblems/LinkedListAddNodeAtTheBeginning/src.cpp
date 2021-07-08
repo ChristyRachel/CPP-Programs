0caad18b14287bb0d8ebecd70a8bec12a447b64e
Christy Rachel Philip <40349694+ChristyRachel@users.noreply.github.com>
2021-07-07 23:52:08 -0400

    To add an element in linked list
    To add a node in the linked list at the beginning of the list.

---
+++ LinkedListProblems/LinkedListAddNodeAtTheBeginning/src.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+class Node {
+public:
+	int value;
+	Node *next;
+};
+
+void PrintLinkedList(Node* head){
+
+	while (head != NULL) {
+		std::cout << (head->value) << " ";
+		head = head->next;
+
+}
+}
+
+void insertNodeAtFront(Node**head, int newValue){
+	Node *newNode = new Node();
+	newNode -> value = newValue;
+	newNode -> next = *head;
+	*head = newNode;
+}
+
+int main() {
+
+	Node *head = new Node();
+	Node *second = new Node();
+	Node *third = new Node();
+	Node *forth = new Node();
+
+	head->value = 20;
+	second->value = 50;
+	third->value = 70;
+	forth->value = 100;
+
+	head->next = second;
+	second->next = third;
+	third->next = forth;
+	forth->next = NULL;
+
+	PrintLinkedList(head);
+
+	insertNodeAtFront(&head,1234);
+
+	return 0;
+}
+
